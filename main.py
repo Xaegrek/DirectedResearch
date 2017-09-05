7d5de07fe0ae202e12885c45124dabb6f9fb90b8
Xaegrek <Xaegrek@gmail.com>
2017-09-05 19:40:29 -0400

    Changed main to allow it to run at startup and not immediately takeoff

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class Main:
 		gVar.altitudeTarget = aAltitudeTarget
 		gVar.flyTime = aFlyTime
 		gVar.GPS = gps
-		# vehicleConnect.connectToUAV()
+		vehicleConnect.connectToUAV()
 
 	def run(self):
 		userControl.userInput()
