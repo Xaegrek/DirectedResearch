1d27dab56d7ac8927d6383e29d97040b80bfb11b
Xaegrek <Xaegrek@gmail.com>
2017-09-18 11:08:12 -0400

    typo in PX4n (loweracse x)

--- include/globalVariables.py
+++ include/globalVariables.py
@@ -5,6 +5,6 @@ UAVS = None
 GPS = None
 
 # Pixhawk ID
-Px4n = 2	# number of possible pixhawk id's, for more general control.  May switch it to autodetect in future
+PX4n = 2	# number of possible pixhawk id's, for more general control.  May switch it to autodetect in future
 PX4ID0 = '/dev/serial/by-id/usb-3D_Robotics_PX4_FMU_v2.x_0-if00'
 PX4ID1 = '/dev/serial/by-id/usb-3D_Robotics_PX4_BL_FMU_v2.x_0-if00'
