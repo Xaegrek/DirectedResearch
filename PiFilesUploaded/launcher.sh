f4a487f1a2c4ec72fe7455ecdbd63e679d802762
Xaegrek <Xaegrek@gmail.com>
2017-12-17 04:43:26 -0500

    Added folder of PI data
    Data contains auto-run scripts used on Raspberry Pi implementation of this codebase, as well as some of the data outputs from running the scripts.

---
+++ PiFilesUploaded/launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# sleep 15 seconds befdore launching  (after start)
+# navigate to home directory, then to script directory, execute script, then return home
+
+sudo ./bin/adhoc_config.sh
+#screen -S uav sudo python MasterThesis/main.py
+$shell
---
+++ PiFilesUploaded/thesisLaunch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+#cd /
+#cd home/amvien/MasterThesis
+#sudo python main.py
+#cd /
+
+sudo python MasterThesis/main.py
+$SHELL
+
+
